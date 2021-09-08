3b55f7db861d268585ba72f01dd40acdb47fc26c
Andrey Shchepkin <lovenfed@gmail.com>
2021-09-08 17:44:35 +0300

    added WebDriverManager

--- src/test/java/IssueDebitCardTest.java
+++ src/test/java/IssueDebitCardTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,6 +18,7 @@ public class IssueDebitCardTest {
 
     @BeforeAll
     static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
         System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
